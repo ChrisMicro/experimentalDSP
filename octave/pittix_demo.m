49d5f979850cca7b548e64b831cfad101421739e
ChrisMicro <christoph@roboterclub-freiburg.de>
2018-03-08 22:21:32 +0100

    demo extended

--- octave/pittix_demo.m
+++ octave/pittix_demo.m
@@ -1,16 +1,34 @@
-play(pittix(440));
-play(pittix(880));
-
 SINUS=0;
 TRIANGLE=1;
 SAWTOOTH=2;
 RECTANGLE=3;
 NOISE=4;
 
-s1=pittix(440,TRIANGLE);
-s2=pittix(440,SAWTOOTH);
-s3=pittix(440,RECTANGLE);
-s4=pittix(440,NOISE);
+s=pittix(440,0);
+play(s);
+
+s1=pittix(440,TRIANGLE); play(s1);
+s2=pittix(440,SAWTOOTH); play(s2);
+s3=pittix(440,RECTANGLE); play(s3);
+s4=pittix(440,NOISE);play(s4);
 
 play([s1,s2,s3,s4]);
-play([s3,s2,s1,s4]);
+
+% amplitude modulation
+lfo=pittix(10);
+play(s1.*lfo);
+play(s2.*lfo);
+play(s3.*lfo);
+play(s4.*lfo);
+
+s1=pittix(440,TRIANGLE); play(s1);
+s2=pittix(523,SAWTOOTH); play(s2);
+s3=pittix(587,RECTANGLE); play(s3);
+s4=pittix(659,NOISE);play(s4);
+
+play([s1,s2,s3,s4]);
+
+% amplitude modulation
+s4=pittix(2000,NOISE,3);play(s4);
+lfo=pittix(10,SAWTOOTH,3);
+play(s4.*lfo);
